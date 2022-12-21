cc63ddcf04178866f577d32a7fb1fdfe0b353d62
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2022-12-21 15:45:07 -0300

    Apenas menu de opções

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("Selecione uma opçao");
+        Console.WriteLine("1 - Abrir arquivo de texto");
+        Console.WriteLine("2 - Criar novo arquivo");
+        Console.WriteLine("0 - Sair");
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 0:
+                Environment.Exit(0);
+                break;
+
+            case 1:
+                Open();
+                break;
+
+            case 2:
+                Create();
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida!");
+                Menu();
+                break;
+        }
+    }
+
+    static void Open()
+    {
+
+    }
+
+    static void Create()
+    {
+
+    }
+}
